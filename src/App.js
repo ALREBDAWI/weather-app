afb0f872762a2405d2b9dcf08837f70435318dfe
ALREBDAWI <mahmoudrebdawi@gmail.com>
2025-03-16 17:48:46 +0100

    init project+fetch weather data ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';  //axios is a library to manage api calls
+
 
 function App() {
+
+    const [data, setData] = useState({});
+    const [location, setLocation] = useState('');
+
+    
+    
+    const searchlocation = async () => {
+        if(!location){
+          console.error('location is not provided')
+        } try {
+            const api_key = process.env.REACT_APP_API_KEY   
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${api_key}`
+            const response = await axios.get(url);
+              setData(response.data);       
+            
+        } catch (error) {
+          console.error("Error fetching data:", error)
+        }
+
+    }
+   
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>weather app</h1>
+      <input
+      type="text"
+      value={location}  
+      onChange={(e) => setLocation(e.target.value)} //use input value to set state
+      />
+      <button onClick={searchlocation}>submit</button> 
+      <div id='city'>city</div>
+      <div id='temp'> temprature : {data.main.feels_like} </div>
+      <div id='humidity'>humidity :</div>
     </div>
   );
 }
