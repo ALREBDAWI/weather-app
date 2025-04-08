c036f5614d143d89c777c20855c7a580ab03dec9
ALREBDAWI <mahmoudrebdawi@gmail.com>
2025-04-08 14:42:47 +0200

    Rounding values

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,9 +2,9 @@ import React from "react";
 export default function details({props}) {
     return(
         <>
-            <div id='temp'> temprature : {props.main?.temp??'Not Available'} </div>   
+            <div id='temp'> temprature : {Math.round(props.main?.temp)??'Not Available'} </div>   
             <div id='humidity'>humidity : {props.main?.humidity !=null ? `${props.main.humidity}%` : 'Not Available'}</div>
-            <div id='feels_like'>feels like : {props.main?.feels_like??"Not Available"}</div>
+            <div id='feels_like'>feels like : {Math.round(props.main?.feels_like)??"Not Available"}</div>
         </>
     )
 };
